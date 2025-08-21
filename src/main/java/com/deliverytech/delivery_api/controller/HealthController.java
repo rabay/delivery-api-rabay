dae0f9af1d4331d23847d1ae2948199facb3eda8
Victor Rabay <>
2025-08-20 21:25:45 -0300

    Ajuste no controller para testes locais de hot-reload.

--- src/main/java/com/deliverytech/delivery_api/controller/HealthController.java
+++ src/main/java/com/deliverytech/delivery_api/controller/HealthController.java
@@ -24,7 +24,7 @@ public class HealthController {
         return new AppInfo(
             "Delivery Tech API",
             "1.0.0",
-            "Seu Nome Aqui",
+            "Victor Alexandre Rabay",
             "JDK 21",
             "Spring Boot 3.2.x"
         );
