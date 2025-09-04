548813ba5e1ef047828487b0ce1ebd7c94fac827
Victor Rabay <>
2025-09-03 22:11:40 -0300

    chore(scripts): remover scripts sensíveis do controle e adicionar ao .gitignore

--- scripts/check_h2_schema.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# script desativado - use os endpoints /db/schema, /db/schema?table=NAME e /db/integrity na aplicação
-
-echo "script desativado - use os endpoints /db/* na aplicação"
--- scripts/test_integrity.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# script desativado - use GET /db/integrity ou POST /db/query na aplicação
-
-echo "script desativado - use os endpoints /db/* na aplicação"
