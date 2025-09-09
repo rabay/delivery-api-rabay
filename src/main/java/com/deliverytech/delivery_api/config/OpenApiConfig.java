385b7ace71883839e8a0a5e2b2853e7920bb17bb
Victor Rabay <>
2025-09-09 20:10:43 -0300

    feat: add JWT Bearer SecurityScheme to OpenAPI config
    - Add SecurityScheme for HTTP Bearer JWT authentication
    - Add SecurityRequirement to apply bearerAuth globally
    - Enable Authorize button in Swagger UI for JWT tokens
    
    Closes TASK-002 from feature-swagger-documentation plan

--- src/main/java/com/deliverytech/delivery_api/config/OpenApiConfig.java
+++ src/main/java/com/deliverytech/delivery_api/config/OpenApiConfig.java
@@ -2,6 +2,9 @@ package com.deliverytech.delivery_api.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +13,16 @@ public class OpenApiConfig {
 
   @Bean
   public OpenAPI customOpenAPI() {
+    SecurityScheme bearerScheme = new SecurityScheme()
+        .type(SecurityScheme.Type.HTTP)
+        .scheme("bearer")
+        .bearerFormat("JWT")
+        .in(SecurityScheme.In.HEADER)
+        .name("Authorization");
+
     return new OpenAPI()
+        .components(new Components().addSecuritySchemes("bearerAuth", bearerScheme))
+        .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
         .info(
             new Info()
                 .title("Delivery API Rabay")
